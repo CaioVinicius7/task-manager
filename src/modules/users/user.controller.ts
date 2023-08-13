b3ae74f072bbeb8e2baf8a30ab62d92e82c2832a
Caio Vinícius <caio1525pereira@gmail.com>
2023-08-13 18:53:05 -0300

    feat: :sparkles: Controller de ataulização de avatar do usuário

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,18 +1,31 @@
 import { Request } from "express";
 
-import { Body, Controller, Get, Post, Req, UseGuards } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Req,
+  UseGuards,
+  UseInterceptors,
+  UploadedFile
+} from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
 
 import { AuthGuard } from "@infra/providers/auth-guard";
 
 import { CreateUserResponseSchema, CreateUserDTO } from "./schemas/create-user";
 import { CreateUserUseCase } from "./use-cases/create-user";
 import { GetUserProfileUseCase } from "./use-cases/get-profile";
+import { UploadUserAvatarUseCase } from "./use-cases/upload-user-avatar";
 
 @Controller("/users")
 export class UserController {
   constructor(
     private readonly createUserUseCase: CreateUserUseCase,
-    private readonly getUserProfileUseCase: GetUserProfileUseCase
+    private readonly getUserProfileUseCase: GetUserProfileUseCase,
+    private readonly uploadUserAvatarUseCase: UploadUserAvatarUseCase
   ) {}
 
   @Post()
@@ -35,5 +48,18 @@ export class UserController {
   @UseGuards(AuthGuard)
   async getProfile(@Req() request: Request) {
     return this.getUserProfileUseCase.execute(request.user.sub);
+  }
+
+  @Patch("/avatar")
+  @UseGuards(AuthGuard)
+  @UseInterceptors(FileInterceptor("file"))
+  async uploadAvatar(
+    @Req() req: Request,
+    @UploadedFile() file: Express.Multer.File
+  ) {
+    return this.uploadUserAvatarUseCase.execute({
+      userId: req.user.sub,
+      avatar: file
+    });
   }
 }
