f2d68c483e6fcfcb57f7f3600f376a07765d13ac
Caio Vinícius <caio1525pereira@gmail.com>
2023-08-14 13:32:03 -0300

    test: :white_check_mark: Testes do caso de uso de recuperação de dados do usuário

---
+++ src/modules/users/use-cases/tests/get-user-profile.spec.ts
@@ -0,0 +1,45 @@
+import { Test } from "@nestjs/testing";
+
+import { InMemoryUserRepository } from "@modules/users/repositories/in-memory/in-memory-user.repository";
+import { UserRepository } from "@modules/users/repositories/user.repository";
+
+import { CreateUserUseCase } from "../create-user";
+import { GetUserProfileUseCase } from "../get-user-profile";
+
+describe("GetUserProfileUseCase", () => {
+  let sut: GetUserProfileUseCase;
+  let createUserUseCase: CreateUserUseCase;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        CreateUserUseCase,
+        GetUserProfileUseCase,
+        {
+          provide: UserRepository,
+          useClass: InMemoryUserRepository
+        }
+      ]
+    }).compile();
+
+    sut = moduleRef.get<GetUserProfileUseCase>(GetUserProfileUseCase);
+    createUserUseCase = moduleRef.get<CreateUserUseCase>(CreateUserUseCase);
+  });
+
+  it("Should be able to get user profile", async () => {
+    const newUser = {
+      name: "Caio",
+      username: "CaioVinícius7",
+      email: "caio@gmail.com",
+      password: "@SuperSecret123"
+    };
+
+    const { user: CreatedUser } = await createUserUseCase.execute(newUser);
+
+    const { user } = await sut.execute(CreatedUser.id);
+
+    expect(user.name).toEqual("Caio");
+    expect(user.username).toEqual("CaioVinícius7");
+    expect(user.email).toEqual("caio@gmail.com");
+  });
+});
