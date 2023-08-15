aed5538973eb9fd03de7c85402ae722941b59bec
Caio Vinícius <caio1525pereira@gmail.com>
2023-08-15 17:28:07 -0300

    feat: :sparkles: Recuperando tasks iniciadas no dia no cron job

--- src/infra/jobs/notification-task-user.schedule.ts
+++ src/infra/jobs/notification-task-user.schedule.ts
@@ -1,10 +1,16 @@
 import { Injectable } from "@nestjs/common";
 import { Cron, CronExpression } from "@nestjs/schedule";
 
+import { TaskRepository } from "@modules/tasks/repositories/task.repository";
+
 @Injectable()
 export class NotificationTaskUserSchedule {
+  constructor(private readonly taskRepository: TaskRepository) {}
+
   @Cron(CronExpression.EVERY_10_SECONDS)
-  getAllTasksDay() {
-    console.log("Running Job");
+  async getAllTasksDay() {
+    const tasks = await this.taskRepository.findAllAfterStartDay();
+
+    console.log(tasks);
   }
 }
--- src/infra/jobs/schedule.module.ts
+++ src/infra/jobs/schedule.module.ts
@@ -1,11 +1,20 @@
 import { Module } from "@nestjs/common";
 import { ScheduleModule } from "@nestjs/schedule";
 
+import { PrismaTaskRepository } from "@modules/tasks/repositories/prisma/task.prisma.repository";
+import { TaskRepository } from "@modules/tasks/repositories/task.repository";
+
 import { NotificationTaskUserSchedule } from "./notification-task-user.schedule";
 
 @Module({
   imports: [ScheduleModule.forRoot()],
   controllers: [],
-  providers: [NotificationTaskUserSchedule]
+  providers: [
+    NotificationTaskUserSchedule,
+    {
+      provide: TaskRepository,
+      useClass: PrismaTaskRepository
+    }
+  ]
 })
 export class ScheduleTaskModule {}
