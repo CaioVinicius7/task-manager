0f6489d5d7b186ca11f25b7421b851caa030af0d
Caio Vinícius <caio1525pereira@gmail.com>
2023-08-15 15:38:46 -0300

    feat: :sparkles: Comunicação com o micro serviço de notificação

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { Module } from "@nestjs/common";
 import { APP_PIPE } from "@nestjs/core";
 
 import { AuthenticationModule } from "./modules/authentication/authentication.module";
+import { NotificationModule } from "./modules/notifications/notification.module";
 import { TasksModule } from "./modules/tasks/tasks.module";
 import { UserModule } from "./modules/users/user.module";
 
 @Module({
-  imports: [TasksModule, UserModule, AuthenticationModule],
+  imports: [NotificationModule, TasksModule, UserModule, AuthenticationModule],
   controllers: [],
   providers: [
     {
