94517a328e515870e72b91d44ae3a148a4ecfa3b
Caio Vinícius <caio1525pereira@gmail.com>
2023-08-16 15:19:24 -0300

    docs: :memo: Base da documentação

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Task Manager")
+    .setDescription("Api for task management")
+    .setVersion("1.0")
+    .setContact(
+      "Caio Vinícius",
+      "https://github.com/caiovinicius7",
+      "caio1525pereira@gmail.com"
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup("api-docs", app, document);
+
   await app.listen(3333);
 }
 
