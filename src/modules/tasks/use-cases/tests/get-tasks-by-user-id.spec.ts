de1b094298c3b57cca55165e18cc808d19bd6525
Caio Vinícius <caio1525pereira@gmail.com>
2023-08-16 19:07:31 -0300

    test: :white_check_mark: Testes do caso de uso de listagem de tarefas de um usuário específico

---
+++ src/modules/tasks/use-cases/tests/get-tasks-by-user-id.spec.ts
@@ -0,0 +1,37 @@
+import { Test } from "@nestjs/testing";
+
+import { InMemoryTaskRepository } from "@modules/tasks/repositories/in-memory/tasks.in-memory.repository";
+import { TasksRepository } from "@modules/tasks/repositories/tasks.repository";
+
+import { GetTasksByUserId } from "../get-tasks-by-user-id";
+
+describe("GetTasksByUserId", () => {
+  let sut: GetTasksByUserId;
+  let tasksRepository: TasksRepository;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        GetTasksByUserId,
+        {
+          provide: TasksRepository,
+          useClass: InMemoryTaskRepository
+        }
+      ]
+    }).compile();
+
+    sut = moduleRef.get<GetTasksByUserId>(GetTasksByUserId);
+    tasksRepository = moduleRef.get<TasksRepository>(TasksRepository);
+  });
+
+  it("Should be able to get a user tasks", async () => {
+    const findByUserIdSpy = jest.spyOn(tasksRepository, "findByUserId");
+
+    const tasks = await sut.execute({
+      userId: "fake-user-id"
+    });
+
+    expect(Array.isArray(tasks)).toBe(true);
+    expect(findByUserIdSpy).toBeCalledWith("fake-user-id");
+  });
+});
