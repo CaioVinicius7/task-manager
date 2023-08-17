e025daccd1468fec5dd4514e7c7200b2b39b56f8
Caio Vinícius <caio1525pereira@gmail.com>
2023-08-17 02:59:10 -0300

    feat: :sparkles: Caso de uso de atualização de tarefa

--- src/modules/tasks/tasks.module.ts
+++ src/modules/tasks/tasks.module.ts
@@ -9,6 +9,7 @@ import { CreateTaskUseCase } from "./use-cases/create-task";
 import { DeleteTaskUseCase } from "./use-cases/delete-task";
 import { GetTaskByIdUseCase } from "./use-cases/get-task-by-id";
 import { GetTasksByUserIdUseCase } from "./use-cases/get-tasks-by-user-id";
+import { UpdateTaskUseCase } from "./use-cases/update-task";
 
 @Module({
   imports: [],
@@ -18,6 +19,7 @@ import { GetTasksByUserIdUseCase } from "./use-cases/get-tasks-by-user-id";
     DeleteTaskUseCase,
     GetTasksByUserIdUseCase,
     GetTaskByIdUseCase,
+    UpdateTaskUseCase,
     {
       provide: TasksRepository,
       useClass: PrismaTasksRepository
---
+++ src/modules/tasks/use-cases/update-task.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@nestjs/common";
+
+import { TasksRepository } from "../repositories/tasks.repository";
+import { TaskNotFound } from "./errors/task-not-found";
+
+interface UpdateTaskRequest {
+  id: string;
+  data: {
+    title?: string;
+    description?: string;
+    priority?: "low" | "medium" | "high";
+    status?: "todo" | "doing" | "done";
+    startAt?: Date;
+    endAt?: Date;
+  };
+}
+
+@Injectable()
+export class UpdateTaskUseCase {
+  constructor(private readonly tasksRepository: TasksRepository) {}
+
+  async execute({ id, data }: UpdateTaskRequest) {
+    const task = await this.tasksRepository.findById(id);
+
+    if (!task) {
+      throw new TaskNotFound();
+    }
+
+    const updatedTask = await this.tasksRepository.update(id, data);
+
+    return updatedTask;
+  }
+}
